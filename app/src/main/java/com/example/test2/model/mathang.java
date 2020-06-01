46a3e05fd6088bd37223e1f84d3db22a64a7ed24
KhanhToan <KhanhToan@DESKTOP-C6JGQK7>
2020-06-01 18:54:19 +0700

    list mathang

--- app/src/main/java/com/example/test2/model/mathang.java
+++ app/src/main/java/com/example/test2/model/mathang.java
@@ -1,9 +1,20 @@
 package com.example.test2.model;
 
 public class mathang {
+    private int id;
     private String donvitinh;
     private long gia;
+
+    public mathang(int id, String donvitinh, long gia, String tenhang) {
+        this.id = id;
+        this.donvitinh = donvitinh;
+        this.gia = gia;
+        this.tenhang = tenhang;
+    }
+
     private String tenhang;
+
+
 
     public String getDonvitinh() { return donvitinh; }
     public void setDonvitinh(String value) { this.donvitinh = value; }
@@ -11,4 +22,12 @@ public class mathang {
     public void setGia(long value) { this.gia = value; }
     public String getTenhang() { return tenhang; }
     public void setTenhang(String value) { this.tenhang = value; }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId()
+    {
+        return id;
+    }
 }
