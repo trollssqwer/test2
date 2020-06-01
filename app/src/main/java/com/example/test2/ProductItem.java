b24ce2ab63f4456db7e87154cb0fbaa64f80fc60
trollssqwer <48431377+trollssqwer@users.noreply.github.com>
2020-06-01 21:02:13 +0700

    gg

--- app/src/main/java/com/example/test2/ProductItem.java
+++ app/src/main/java/com/example/test2/ProductItem.java
@@ -19,128 +19,7 @@ import java.util.ListIterator;
 public class ProductItem extends AppCompatActivity {
     private TextView ProductNamex , ProductPrice ,ProductDescrption;
     private ImageView ProductImage;
-    List<mathang> list = new List<mathang>() {
-        @Override
-        public int size() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean contains(@Nullable Object o) {
-            return false;
-        }
-
-        @NonNull
-        @Override
-        public Iterator<mathang> iterator() {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @NonNull
-        @Override
-        public <T> T[] toArray(@NonNull T[] a) {
-            return null;
-        }
-
-        @Override
-        public boolean add(mathang mathang) {
-            return false;
-        }
-
-        @Override
-        public boolean remove(@Nullable Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean containsAll(@NonNull Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(@NonNull Collection<? extends mathang> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(int index, @NonNull Collection<? extends mathang> c) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(@NonNull Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean retainAll(@NonNull Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @Override
-        public mathang get(int index) {
-            return null;
-        }
-
-        @Override
-        public mathang set(int index, mathang element) {
-            return null;
-        }
-
-        @Override
-        public void add(int index, mathang element) {
-
-        }
-
-        @Override
-        public mathang remove(int index) {
-            return null;
-        }
-
-        @Override
-        public int indexOf(@Nullable Object o) {
-            return 0;
-        }
-
-        @Override
-        public int lastIndexOf(@Nullable Object o) {
-            return 0;
-        }
-
-        @NonNull
-        @Override
-        public ListIterator<mathang> listIterator() {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public ListIterator<mathang> listIterator(int index) {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public List<mathang> subList(int fromIndex, int toIndex) {
-            return null;
-        }
-    };
+    public List<mathang> listA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -151,6 +30,7 @@ public class ProductItem extends AppCompatActivity {
         ProductNamex = (TextView) findViewById(R.id.product_name);
         ProductPrice = (TextView) findViewById(R.id.product_price);
         ProductDescrption = (TextView) findViewById(R.id.product_description);
+
 
         ProductImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -169,7 +49,7 @@ public class ProductItem extends AppCompatActivity {
         mathang a=new mathang(1,"Cốc",10000,"Capuchino");
         mathang b=new mathang(2, "Chai", 1000, "Trà sữa");
 
-        list.add(a);
-        list.add(b);
+        listA.add(a);
+        listA.add(b);
     }
 }
