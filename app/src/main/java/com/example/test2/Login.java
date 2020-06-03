5a9d2efa4ef54dbffcb618b691aba082c9be0494
KhanhToan <KhanhToan@DESKTOP-C6JGQK7>
2020-06-03 15:55:07 +0700

    abc

--- app/src/main/java/com/example/test2/Login.java
+++ app/src/main/java/com/example/test2/Login.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -12,11 +13,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.test2.model.khachhang;
+import com.example.test2.model.mathang;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class Login extends AppCompatActivity {
     private Button LoginButton;
     private EditText UsernameEditText, PasswordEditText;
     private TextView CheckAdminTextView , NoCheckAdminTextView;
+    String username,password;
     ProgressDialog loadingBar;
+    ArrayList<khachhang> listkh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +45,7 @@ public class Login extends AppCompatActivity {
         CheckAdminTextView = (TextView) findViewById(R.id.login_admin_check);
         NoCheckAdminTextView = (TextView) findViewById(R.id.login_admin_nocheck);
         loadingBar = new ProgressDialog(this);
+
 
         CheckAdminTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,8 +74,8 @@ public class Login extends AppCompatActivity {
     }
 
     private void LoginUser(){
-        String username = UsernameEditText.getText().toString();
-        String password = PasswordEditText.getText().toString();
+        username = UsernameEditText.getText().toString();
+        password = PasswordEditText.getText().toString();
 
         if(TextUtils.isEmpty(password)){
             Toast.makeText(this,"Hay dien mat khau",Toast.LENGTH_LONG).show();
@@ -71,27 +90,89 @@ public class Login extends AppCompatActivity {
             loadingBar.show();
 
             // check database
-            ValidateLogin(username,password);
+            ValidateLogin();
         }
     }
 
     //Check database Login
-    private void ValidateLogin(String username,String password){
-        Intent intenta = new Intent(Login.this,ShopList.class);
-        startActivity(intenta);
+    private void ValidateLogin(){
+        //check user, passwrod
 
-        //Check usename, password
-//        if(username != "a"){
-//            Toast.makeText(Login.this,"Ten dang nhap hoac mat khau sai !",Toast.LENGTH_LONG).show();
-//            Intent intent = new Intent(Login.this,AdminCategory.class);
-//            startActivity(intent);
-//        }
-//        else{
-//            Toast.makeText(Login.this,"Dang nhap thanh cong",Toast.LENGTH_LONG).show();
-//            Intent intent = new Intent(Login.this,Home.class);
-//            startActivity(intent);
-//        }
+        new getURL().execute("http://35.198.237.116/coffeshop/api/Khachhangs");
     }
+    class getURL extends AsyncTask<String,Void,String>
+    {
+        OkHttpClient client=new OkHttpClient.Builder().connectTimeout(15, TimeUnit.SECONDS)
+                .writeTimeout(15,TimeUnit.SECONDS)
+                .readTimeout(15,TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .build();
+        @Override
+        protected String doInBackground(String... strings) {
+            Request.Builder builder = new Request.Builder();
+            builder.url(strings[0]);
+            Request request=builder.build();
+            try {
+                Response response= client.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(!s.equals(""))
+            {
+                try {
+                    JSONArray array=new JSONArray(s);
+                    for(int i=0;i<array.length();i++)
+                    {
+                        khachhang kh=new khachhang();
+                        JSONObject object=array.getJSONObject(i);
+                        kh.setIdkhachhang(object.getInt("idkhachhang"));
+                        kh.setEmail(object.getString("email"));
+                        kh.setFacebook(object.getString("facebook"));
+                        kh.setSodienthoai(object.getString("sodienthoai"));
+                        kh.setTenkhachhang(object.getString("tenkhachhang"));
+                        listkh.add(kh);
+
+                    }
+                    boolean kt=false;
+                    for(int i=0;i<listkh.size();i++)
+                    {
+                        if(listkh.get(i).getSodienthoai()==username)
+                        {
+                            kt=true;
+                            break;
+                        }
+                        kt=false;
+                    }
+                    if(kt=true)
+                    {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent intenta = new Intent(Login.this,ShopList.class);
+                                startActivity(intenta);
+                            }
+                        });
+
+                    }
+                    else
+                    {
+                        Toast.makeText(Login.this,"Tài khoản không đúng",Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            super.onPostExecute(s);
+        }
+    }
 
 }
