2ed7c737ff18dbeb81659bc5636c855dbd97ab48
trollssqwer <48431377+trollssqwer@users.noreply.github.com>
2020-06-03 09:14:17 +0700

    gg

---
+++ app/src/main/java/com/example/test2/CartAdapter.java
@@ -0,0 +1,78 @@
+package com.example.test2;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.test2.model.mathang;
+
+import java.util.ArrayList;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder> {
+    private ArrayList<mathang> listHang;
+    private OnProductClickLisner Mlisner;
+    public interface OnProductClickLisner{
+        void OnItemClick(int position);
+    }
+
+    public void setOnPruductClickLisner(OnProductClickLisner lisner){
+        Mlisner =lisner;
+
+    }
+
+    public  static class CartViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imgProduct;
+        private TextView nameProduct;
+        private TextView desProduct;
+        private TextView priceProduct;
+        public CartViewHolder(View itemView, final OnProductClickLisner lisner){
+            super(itemView);
+            imgProduct = itemView.findViewById(R.id.image_cart);
+            nameProduct = itemView.findViewById(R.id.name_cart);
+            desProduct = itemView.findViewById(R.id.price_cart);
+            priceProduct = itemView.findViewById(R.id.count_cart);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(lisner != null){
+                        int position = getAdapterPosition();
+                        if(position != RecyclerView.NO_POSITION){
+                            lisner.OnItemClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public CartAdapter(ArrayList<mathang> list){
+        listHang = list;
+    }
+    @NonNull
+    @Override
+    public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_cartview,parent,false);
+        CartViewHolder pvh = new CartViewHolder(v,Mlisner);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
+        mathang currentProdcut = listHang.get(position);
+
+        holder.imgProduct.setImageResource(R.drawable.ic_launcher_background);
+        holder.priceProduct.setText(currentProdcut.getTenhang());
+        holder.nameProduct.setText(currentProdcut.getDonvitinh());
+        holder.desProduct.setText(String.valueOf(currentProdcut.getGia()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listHang.size();
+    }
+
+
+}
--- app/src/main/java/com/example/test2/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/test2/ui/gallery/GalleryFragment.java
@@ -12,20 +12,55 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.test2.Cart;
+import com.example.test2.InformationProduct;
+import com.example.test2.ProductAdapter;
 import com.example.test2.R;
+import com.example.test2.model.mathang;
+import com.example.test2.ui.home.HomeViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import com.example.test2.CartAdapter;
+
+import static com.example.test2.InformationProduct.listCart;
+
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+    private HomeViewModel ChomeViewModel;
+    private RecyclerView CrecyclerView;
+    private CartAdapter Cadapter;
+    private RecyclerView.LayoutManager ClayoutManager;
+    ArrayList<mathang> ClistHang = new ArrayList<mathang>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        ClistHang.add(new mathang(1,"a",3,"a"));
+        ClistHang.add(new mathang(2,"b",4,"b"));
+        ClistHang.add(new mathang(3,"c",5,"c"));
+        CrecyclerView =  root.findViewById(R.id.cart_list);
+        CrecyclerView.setHasFixedSize(true);
+        ClayoutManager = new LinearLayoutManager(getActivity());
+        Cadapter = new CartAdapter(listCart);
+        CrecyclerView.setLayoutManager(ClayoutManager);
+        CrecyclerView.setAdapter(Cadapter);
+
+
+
+
+
+
+
+
 
 
         return root;
