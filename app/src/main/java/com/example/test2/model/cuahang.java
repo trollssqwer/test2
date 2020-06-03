f124810b7d77349c67c219f8866d1392e56cb244
trollssqwer <48431377+trollssqwer@users.noreply.github.com>
2020-06-03 14:33:19 +0700

    gg

---
+++ app/src/main/java/com/example/test2/model/cuahang.java
@@ -0,0 +1,47 @@
+package com.example.test2.model;
+
+public class cuahang {
+    int macuahang;
+    String anhcuahang;
+    String tencuahang;
+    String diachicuahang;
+
+    public cuahang(int macuahang, String anhcuahang, String tencuahang, String diachicuahang) {
+        this.macuahang = macuahang;
+        this.anhcuahang = anhcuahang;
+        this.tencuahang = tencuahang;
+        this.diachicuahang = diachicuahang;
+    }
+
+    public int getMacuahang() {
+        return macuahang;
+    }
+
+    public void setMacuahang(int macuahang) {
+        this.macuahang = macuahang;
+    }
+
+    public String getAnhcuahang() {
+        return anhcuahang;
+    }
+
+    public void setAnhcuahang(String anhcuahang) {
+        this.anhcuahang = anhcuahang;
+    }
+
+    public String getTencuahang() {
+        return tencuahang;
+    }
+
+    public void setTencuahang(String tencuahang) {
+        this.tencuahang = tencuahang;
+    }
+
+    public String getDiachicuahang() {
+        return diachicuahang;
+    }
+
+    public void setDiachicuahang(String diachicuahang) {
+        this.diachicuahang = diachicuahang;
+    }
+}
